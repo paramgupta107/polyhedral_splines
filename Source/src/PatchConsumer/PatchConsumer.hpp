c04d85ec08ff793da63016362082a2119736314d
jorg@cise.ufl.edu <jorg@cise.ufl.edu>
2021-12-17 21:18:52 +0000

    PatchConsumer.hpp add to commented code snippet (u-derivative)

--- Source/src/PatchConsumer/PatchConsumer.hpp
+++ Source/src/PatchConsumer/PatchConsumer.hpp
@@ -9,17 +9,20 @@
  *  what we do with the patches. e.g. customize output format.
  *  We use BVWriter by default and provide IGSWriter as another example.
  *
- *  Other usage e.g. derivative of BB-patch along u direction:
- *  User can also add following code into customized writer to get differentiated patch.
+ *  Other Example (commented code snippet): 
+ *     compute the derivative of a BB-patch in the u direction
+ *     by scaling, by the degree in u, the difference of BB-coefficients  [i][j] and [i-1][j]
+ *     in a double loop over all BB-coefficients (that have an [i-1] neighbor)
  *
  *  std::vector<std::vector<double>> get_du(const Patch& a_Patch)
  *  {
  *      std::vector<std::vector<double>> t_Du;
- *      for(int i=1; i<a_Patch.m_DegU+1; i++)
+ *      for(int i=1; i<a_Patch.m_DegU+1; i++)   // note: start at 1 so that [i-1] is well-defined
  *      {
  *          std::vector<double> t_DuRow;
  *          for(int j=0; j<a_Patch.m_DegV+1; j++)
  *          {
+ *              // scaling, by the degree in u, the difference of BB-coefficients  [i][j] and [i-1][j]
  *              t_DuRow.push_back(a_Patch.m_DegU * (a_Patch.m_BBcoefs[i][j] - a_Patch.m_BBcoefs[i-1][j]));
  *          }
  *          t_Du.push_back(t_DuRow);
